5aed35f6211550306e05d4f2f9243432c2c6ed5e
divya1490 <71721811+divya1490@users.noreply.github.com>
2020-09-22 22:16:25 +0530

    Add files via upload

---
+++ Basic R.R
@@ -0,0 +1,18 @@
+# Assignment Operators
+# 1. Left Assignment <- or == or <<-
+v1 <- c(3,1,TRUE)
+v2 <<- c(3,1,TRUE)
+v3 = c(3,1,FALSE)
+v1
+v2
+v3
+# 2. Right Assignment -> or ->>
+c(3,1,TRUE,2+3i) -> v4
+c(3,1,TRUE,2+3i) ->> v5
+v4
+v5
+# 1. greater  than
+q <- 1
+r <- 4
+q[1] > r[4]
+1>4
